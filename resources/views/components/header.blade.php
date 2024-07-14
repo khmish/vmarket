a8faae7050fa6fb77dc134f6c4f38e0e0e1a0084
khmish <30586743+khmish@users.noreply.github.com>
2024-07-14 23:36:14 +0300

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,14 @@
 
 
-<div class="relative isolate overflow-hidden bg-gray-900 py-10 sm:py-12">
+<div class="relative isolate overflow-hidden bg-red-100  py-10 sm:py-12">
     
    
     <div class="mx-auto max-w-7xl px-5 lg:px-6">
     
       <div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none ">
-        <div class="grid grid-cols-1 text-base font-semibold text-white sm:grid-cols-2 md:flex lg:gap-x-10">
+        <div class="grid grid-cols-1 text-base font-semibold text-black sm:grid-cols-2 md:flex lg:gap-x-10">
           <a href="/vendor-product">Vendor products </a>
-          <a href="#">Internship program </a>
+          <a href="/home">home </a>
         </div>
         
       </div>
