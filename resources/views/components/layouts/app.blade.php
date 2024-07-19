1b5cc65e1eeedd96cbed4283b0675df449a85018
khmish <30586743+khmish@users.noreply.github.com>
2024-07-20 00:13:00 +0300

    add google analytics

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,6 +10,25 @@
 
     <title>{{ config('app.name') }}</title>
 
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-WMF7Q9Q3');
+    </script>
+    <!-- End Google Tag Manager -->
+
     <style>
         [x-cloak] {
             display: none !important;
@@ -38,6 +57,10 @@
     @filamentScripts
     @livewireScripts
     @vite('resources/js/app.js')
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMF7Q9Q3" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 </body>
 
 </html>
