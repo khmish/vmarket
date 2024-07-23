a1a69d6a5cfb2bb9346c5369926aa5f0321c7bc6
khmish <30586743+khmish@users.noreply.github.com>
2024-07-23 06:55:12 +0300

    language switcher

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
+use BezhanSalleh\FilamentLanguageSwitch\Enums\Placement;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +21,23 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['ar','en']) // also accepts a closure
+                ->visible(outsidePanels: true)
+                ->outsidePanelRoutes([
+                    'profile',
+                    'home',
+                    '/',
+                    // Additional custom routes where the switcher should be visible outside panels
+                ])
+                ->flags([
+                    'ar' => asset('flags/saudi-arabia.svg'),
+                    'en' => asset('flags/usa.svg'),
+                ])
+                ->flagsOnly()
+                ->circular()
+                ->outsidePanelPlacement(Placement::TopCenter);
+        });
     }
 }
